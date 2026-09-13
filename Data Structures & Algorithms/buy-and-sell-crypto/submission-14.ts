40c377e5abae01d84d0c76ebfbcb4c479b0a7f54
radha260102-cmyk <radha260102@gmail.com>
2026-09-13 14:08:05 +0530

    Add: buy-and-sell-crypto - submission-14

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-14.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        // const sortedArr = prices.sort((a,b)=>a-b);
+        let buyPrice = prices[0];
+        let sellPrice = prices[0];
+        let MajorDiff = sellPrice - buyPrice;
+        let MajorDiff1 = sellPrice - buyPrice;
+        for(const p in prices){
+            if(prices[p] !== 0){}
+            // console.log(sellPrice, buyPrice, prices[p],MajorDiff)
+            // let diff = 
+            if(prices[p] < buyPrice ){
+                
+                buyPrice = prices[p];
+                sellPrice = prices[p];
+                
+                 
+            }else if(prices[p] > sellPrice){
+                // console.log('inside laa')
+                sellPrice = prices[p];
+                MajorDiff1 = MajorDiff;
+                MajorDiff = sellPrice - buyPrice;
+
+            }
+            
+
+        }
+
+        return Math.max(MajorDiff1, MajorDiff);
+
+    }
+}
